c420e01113b35593f07bdc12c79096370935936d
YuQing <384681@qq.com>
2018-10-10 18:53:28 +0800

    index template render OK

--- src/nginx-module/ngx_http_fastken_module.c
+++ src/nginx-module/ngx_http_fastken_module.c
@@ -9,7 +9,8 @@
 #include "fastken/fken_client.h"
 #include "template.h"
 
-#define MAX_PARAM_COUNT  5
+#define MAX_QUESTION_SIZE 64
+#define MAX_PARAM_COUNT    5
 
 #define START_MARK_STR  "=[["
 #define END_MARK_STR    "]]"
@@ -19,8 +20,35 @@
 #define PARAM_NAME_QUESTION_STR  "question"
 #define PARAM_NAME_QUESTION_LEN  (sizeof(PARAM_NAME_QUESTION_STR) - 1)
 
-#define PARAM_NAME_VARS_STR      "vars"
-#define PARAM_NAME_VARS_LEN      (sizeof(PARAM_NAME_VARS_STR) - 1)
+#define PARAM_NAME_ANSWER_STR    "answer"
+#define PARAM_NAME_ANSWER_LEN    (sizeof(PARAM_NAME_ANSWER_STR) - 1)
+
+#define PARAM_NAME_DISPLAY_ANSWER_STR  "display_answer"
+#define PARAM_NAME_DISPLAY_ANSWER_LEN  (sizeof(PARAM_NAME_DISPLAY_ANSWER_STR) - 1)
+
+#define PARAM_NAME_VARS_STR    "vars"
+#define PARAM_NAME_VARS_LEN    (sizeof(PARAM_NAME_VARS_STR) - 1)
+
+#define PARAM_NAME_OSNAME_STR  "osname"
+#define PARAM_NAME_OSNAME_LEN  (sizeof(PARAM_NAME_OSNAME_STR) - 1)
+
+#define PARAM_NAME_UNAME_STR   "uname"
+#define PARAM_NAME_UNAME_LEN   (sizeof(PARAM_NAME_UNAME_STR) - 1)
+
+#define OSNAME_CENTOS_STR  "CentOS"
+#define OSNAME_CENTOS_LEN  (sizeof(OSNAME_CENTOS_STR) - 1)
+
+#define OSNAME_UBUNTU_STR  "Ubuntu"
+#define OSNAME_UBUNTU_LEN  (sizeof(OSNAME_UBUNTU_STR) - 1)
+
+#define OSNAME_DARWIN_STR  "Darwin"
+#define OSNAME_DARWIN_LEN  (sizeof(OSNAME_DARWIN_STR) - 1)
+
+#define UNAME_LINUX_STR    "Linux"
+#define UNAME_LINUX_LEN    (sizeof(UNAME_LINUX_STR) - 1)
+
+#define UNAME_DARWIN_STR    OSNAME_DARWIN_STR
+#define UNAME_DARWIN_LEN    (sizeof(UNAME_DARWIN_STR) - 1)
 
 #define URI_ROOT_PATH_STR      "/fastken"
 #define URI_ROOT_PATH_LEN      (sizeof(URI_ROOT_PATH_STR) - 1)
@@ -30,6 +58,9 @@
 
 #define URI_SEARCH_UNIX_STR     "/search/unix"
 #define URI_SEARCH_UNIX_LEN     (sizeof(URI_SEARCH_UNIX_STR) - 1)
+
+#define EMPTY_RESP_STR   "没有找到你想要的内容，换个问题试试？\n"
+#define EMPTY_RESP_LEN   (sizeof(EMPTY_RESP_STR) - 1)
 
 typedef struct {
 	ngx_http_upstream_conf_t   upstream;
@@ -41,10 +72,25 @@ static FKenClient client;
 
 static string_t start_mark = {START_MARK_STR, START_MARK_LEN};
 static string_t end_mark = {END_MARK_STR, END_MARK_LEN};
+
 static string_t param_name_question = {PARAM_NAME_QUESTION_STR,
     PARAM_NAME_QUESTION_LEN};
 static string_t param_name_vars = {PARAM_NAME_VARS_STR,
     PARAM_NAME_VARS_LEN};
+static string_t param_name_osname = {PARAM_NAME_OSNAME_STR,
+    PARAM_NAME_OSNAME_LEN};
+static string_t param_name_uname = {PARAM_NAME_UNAME_STR,
+    PARAM_NAME_UNAME_LEN};
+
+static string_t uname_linux = {UNAME_LINUX_STR, UNAME_LINUX_LEN};
+static string_t uname_darwin = {UNAME_DARWIN_STR, UNAME_DARWIN_LEN};
+
+static string_t param_name_answer = {PARAM_NAME_ANSWER_STR,
+    PARAM_NAME_ANSWER_LEN};
+static string_t param_name_display_answer = {PARAM_NAME_DISPLAY_ANSWER_STR,
+    PARAM_NAME_DISPLAY_ANSWER_LEN};
+
+static string_t string_none = {"none", 4};
 
 static char *ngx_http_fastken_set(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 //static ngx_int_t ngx_http_fastken_init(ngx_conf_t *cf);
@@ -93,7 +139,7 @@ ngx_module_t  ngx_http_fastken_module = {
 #include "template.c"
 
 static ngx_int_t fken_send_reply_chunk(ngx_http_request_t *r,
-        const bool last_buf, const char *buff, const int size,
+        const bool last_buf, const string_t *buff,
         const bool duplicate)
 {
 	ngx_buf_t *b;
@@ -109,22 +155,22 @@ static ngx_int_t fken_send_reply_chunk(ngx_http_request_t *r,
 	}
 
     if (duplicate) {
-        new_buff = ngx_palloc(r->pool, sizeof(u_char) * size);
+        new_buff = ngx_palloc(r->pool, buff->len);
         if (new_buff == NULL) {
             ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
-                    "ngx_palloc %d bytes fail", size);
+                    "ngx_palloc %d bytes fail", buff->len);
             return NGX_HTTP_INTERNAL_SERVER_ERROR;
         }
-        memcpy(new_buff, buff, size);
+        memcpy(new_buff, buff->str, buff->len);
     } else {
-        new_buff = (u_char *)buff;
+        new_buff = (u_char *)buff->str;
     }
 
 	out.buf = b;
 	out.next = NULL;
 
 	b->pos = new_buff;
-	b->last = new_buff + size;
+	b->last = new_buff + buff->len;
 	b->memory = 1;
 	b->last_in_chain = last_buf;
 	b->last_buf = last_buf;
@@ -286,7 +332,7 @@ static ngx_int_t set_http_header(ngx_http_request_t *r,
     u_char *new_buff;
     ngx_table_elt_t  *cc;
 
-	new_buff = ngx_palloc(r->pool, sizeof(u_char) * (value_len + 1));
+	new_buff = ngx_palloc(r->pool, (value_len + 1));
 	if (new_buff == NULL) {
 		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
 			"ngx_palloc %d bytes fail", value_len + 1);
@@ -326,71 +372,43 @@ static inline ngx_int_t set_header_answer_count(ngx_http_request_t *r,
             buff, value_len);
 }
 
-static ngx_int_t send_response(ngx_http_request_t *r,
-        FKenAnswerArray *answer_array)
+static int format_answer_output(ngx_http_request_t *r,
+        FKenAnswerArray *answer_array,
+        string_t *output)
 {
-#define CONTENT_TYPE_TEXT_STR   "text/plain"
-#define CONTENT_TYPE_TEXT_LEN  (sizeof(CONTENT_TYPE_TEXT_STR) - 1)
-
 #define ANSWER_SEPERATOR_STR   "\n==========answer split line==========\n"
 #define ANSWER_SEPERATOR_LEN   (sizeof(ANSWER_SEPERATOR_STR) - 1)
 
 #define ANSER_ID_BUFF_SIZE    64
 
-#define EMPTY_RESP_STR   "没有找到你想要的内容，换个问题试试？\n"
-#define EMPTY_RESP_LEN   (sizeof(EMPTY_RESP_STR) - 1)
-
-    char *out_content;
-    ngx_int_t rc;
-    int content_len;
     char id_buffs[FKEN_MAX_ANSWER_COUNT][ANSER_ID_BUFF_SIZE];
     string_t answer_ids[FKEN_MAX_ANSWER_COUNT];
+    char *p;
     int i;
 
     if (answer_array->count > 0) {
-        content_len = 0;
+        output->len = 0;
         for (i=0; i<answer_array->count; i++) {
             if (i > 0) {
-                content_len += ANSWER_SEPERATOR_LEN;
+                output->len += ANSWER_SEPERATOR_LEN;
             }
 
             answer_ids[i].str = id_buffs[i];
             answer_ids[i].len = snprintf(answer_ids[i].str,
                     ANSER_ID_BUFF_SIZE, "question id: %"PRId64"\n\n",
                     answer_array->answers[i].id);
-            content_len += answer_ids[i].len + answer_array->
+            output->len += answer_ids[i].len + answer_array->
                 answers[i].answer.len;
         }
 
-        out_content = (char *)ngx_palloc(r->pool, sizeof(u_char) * content_len);
-        if (out_content == NULL) {
+        output->str = (char *)ngx_palloc(r->pool, output->len);
+        if (output->str == NULL) {
             ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
-                    "ngx_palloc %d bytes fail", content_len);
-            return NGX_HTTP_INTERNAL_SERVER_ERROR;
+                    "ngx_palloc %d bytes fail", output->len);
+            return ENOMEM;
         }
-    } else {
-        out_content = NULL;
-        content_len = EMPTY_RESP_LEN;
-    }
 
-    set_header_answer_count(r, answer_array->count);
-    r->headers_out.content_type.len = CONTENT_TYPE_TEXT_LEN;
-    r->headers_out.content_type.data = (u_char *)CONTENT_TYPE_TEXT_STR;
-    r->headers_out.content_length_n = content_len;
-    r->headers_out.status = NGX_HTTP_OK;
-
-    ngx_http_set_content_type(r);
-
-    rc = ngx_http_send_header(r);
-    if (rc == NGX_ERROR || rc > NGX_OK) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
-                "ngx_http_send_header fail, return code: %d", rc);
-        return rc;
-    }
-
-    if (answer_array->count > 0) {
-        char *p;
-        p = out_content;
+        p = output->str;
         for (i=0; i<answer_array->count; i++) {
             if (i > 0) {
                 memcpy(p, ANSWER_SEPERATOR_STR, ANSWER_SEPERATOR_LEN);
@@ -403,13 +421,43 @@ static ngx_int_t send_response(ngx_http_request_t *r,
                     answer_array->answers[i].answer.len);
             p += answer_array->answers[i].answer.len;
         }
-
-        return fken_send_reply_chunk(r, true,
-                out_content, content_len, false);
     } else {
-        return fken_send_reply_chunk(r, true,
-                EMPTY_RESP_STR, EMPTY_RESP_LEN, false);
+        output->str = EMPTY_RESP_STR;
+        output->len = EMPTY_RESP_LEN;
     }
+
+    return 0;
+}
+
+static ngx_int_t send_response(ngx_http_request_t *r,
+        FKenAnswerArray *answer_array)
+{
+#define CONTENT_TYPE_TEXT_STR   "text/plain"
+#define CONTENT_TYPE_TEXT_LEN  (sizeof(CONTENT_TYPE_TEXT_STR) - 1)
+
+    string_t output;
+    ngx_int_t rc;
+
+    if (format_answer_output(r, answer_array, &output) != 0) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    set_header_answer_count(r, answer_array->count);
+    r->headers_out.content_type.len = CONTENT_TYPE_TEXT_LEN;
+    r->headers_out.content_type.data = (u_char *)CONTENT_TYPE_TEXT_STR;
+    r->headers_out.content_length_n = output.len;
+    r->headers_out.status = NGX_HTTP_OK;
+
+    ngx_http_set_content_type(r);
+
+    rc = ngx_http_send_header(r);
+    if (rc == NGX_ERROR || rc > NGX_OK) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                "ngx_http_send_header fail, return code: %d", rc);
+        return rc;
+    }
+
+    return fken_send_reply_chunk(r, true, &output, false);
 }
 
 static void get_answer_ids_string(FKenAnswerArray *answer_array, char *buff)
@@ -434,27 +482,46 @@ static void get_answer_ids_string(FKenAnswerArray *answer_array, char *buff)
     *p = '\0';
 }
 
+static int fastken_search_do(ngx_http_request_t *r, string_t *question,
+        const char *body, key_value_array_t *var_array,
+        FKenAnswerArray *answer_array)
+{
+    int result;
+    char answer_ids[32 * FKEN_MAX_ANSWER_COUNT];
+
+    if ((result=fken_client_question_search(&client, question,
+                    var_array->kv_pairs, var_array->count, answer_array)) != 0)
+    {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                "question_search result: %d", result);
+        return result;
+    }
+
+    get_answer_ids_string(answer_array, answer_ids);
+    ngx_log_error(NGX_LOG_INFO, r->connection->log, 0,
+            "answers: %d [%s] %s", answer_array->count,
+            answer_ids, body);
+    return 0;
+}
+
 static void ngx_http_fastken_search_handler(ngx_http_request_t *r)
 {
-#define MAX_QUESTION_SIZE 64
-
     ngx_int_t rc;
     key_value_pair_t params[FKEN_MAX_CONDITION_COUNT];
     int param_count;
-	char content[1 * 1024];
+	char body[1 * 1024];
     string_t cont;
     string_t *question;
     string_t *cond;
     FKenAnswerArray answer_array;
-    char answer_ids[32 * FKEN_MAX_ANSWER_COUNT];
+    key_value_array_t var_array;
     key_value_pair_t vars[FKEN_MAX_CONDITION_COUNT];
     int var_count;
-    int result;
     //int i;
 
-    cont.str = content;
+    cont.str = body;
     cont.len = 0;
-    if (get_post_content(r, &cont, sizeof(content)) != NGX_OK) {
+    if (get_post_content(r, &cont, sizeof(body)) != NGX_OK) {
         ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
         return;
     }
@@ -499,19 +566,14 @@ static void ngx_http_fastken_search_handler(ngx_http_request_t *r)
         */
     }
 
-    if ((result=fken_client_question_search(&client, question,
-                    vars, var_count, &answer_array)) != 0)
+    var_array.kv_pairs = vars;
+    var_array.count = var_count;
+    if (fastken_search_do(r, question, body, &var_array,
+                &answer_array) != 0)
     {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
-                "question_search result: %d", result);
         ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
         return;
     }
-
-    get_answer_ids_string(&answer_array, answer_ids);
-    ngx_log_error(NGX_LOG_INFO, r->connection->log, 0,
-            "answers: %d [%s] %s", answer_array.count,
-            answer_ids, content);
 
     rc = send_response(r, &answer_array);
     ngx_http_finalize_request(r, rc);
@@ -532,7 +594,77 @@ static void fastken_copy_params_to_kv_array(const KeyValuePairEx *params,
     kv_array->count = param_count;
 }
 
-static ngx_int_t fastken_index_do(ngx_http_request_t *r,
+static inline void fastken_add_param_to_kv_array(key_value_array_t *kv_array,
+        string_t *key, string_t *value)
+{
+    kv_array->kv_pairs[kv_array->count].key = *key;
+    kv_array->kv_pairs[kv_array->count].value = *value;
+    kv_array->count++;
+}
+
+static int fastken_index_format_vars(key_value_array_t *kv_array,
+        key_value_array_t *var_array)
+{
+    string_t *osname;
+    string_t *uname;
+
+    osname = get_param(kv_array->kv_pairs, kv_array->count, &param_name_osname);
+    if (osname == NULL) {
+        return ENOENT;
+    }
+
+    if (fc_string_equal2(osname, OSNAME_CENTOS_STR, OSNAME_CENTOS_LEN) ||
+            fc_string_equal2(osname, OSNAME_UBUNTU_STR, OSNAME_UBUNTU_LEN))
+    {
+        uname = &uname_linux;
+    } else if (fc_string_equal2(osname, OSNAME_DARWIN_STR, OSNAME_DARWIN_LEN)) {
+        uname = &uname_darwin;
+    } else {
+        uname = &empty_string;
+    }
+
+    fastken_add_param_to_kv_array(var_array, &param_name_osname, osname);
+    fastken_add_param_to_kv_array(var_array, &param_name_uname, uname);
+    return 0;
+}
+
+static int fastken_index_search(ngx_http_request_t *r,
+        key_value_array_t *kv_array,
+        const char *body, string_t *question)
+{
+    string_t output;
+    key_value_array_t var_array;
+    key_value_pair_t vars[FKEN_MAX_CONDITION_COUNT];
+    FKenAnswerArray answer_array;
+
+    if (question->len > MAX_QUESTION_SIZE) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                "question length: %d exceeds %d",
+                question->len, MAX_QUESTION_SIZE);
+        return NGX_HTTP_BAD_REQUEST;
+    }
+
+    var_array.kv_pairs = vars;
+    var_array.count = 0;
+    if (fastken_index_format_vars(kv_array, &var_array) != 0) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    if (fastken_search_do(r, question, body, &var_array,
+                &answer_array) != 0)
+    {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    if (format_answer_output(r, &answer_array, &output) != 0) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    fastken_add_param_to_kv_array(kv_array, &param_name_answer, &output);
+    return NGX_OK;
+}
+
+static ngx_int_t fastken_index_do(ngx_http_request_t *r, const char *body,
         const KeyValuePairEx *params, const int param_count)
 {
 #define CONTENT_TYPE_HTML_STR   "text/html; charset=UTF-8"
@@ -540,9 +672,9 @@ static ngx_int_t fastken_index_do(ngx_http_request_t *r,
 
 	ngx_int_t rc;
     string_t output;
-    key_value_pair_t kvs[MAX_PARAM_COUNT + 1];
+    key_value_pair_t kvs[MAX_PARAM_COUNT + 2];
     key_value_array_t kv_array;
-
+    string_t *question;
 
     logInfo("param_count: %d", param_count);
     {
@@ -554,6 +686,16 @@ static ngx_int_t fastken_index_do(ngx_http_request_t *r,
 
     kv_array.kv_pairs = kvs;
     fastken_copy_params_to_kv_array(params, param_count, &kv_array);
+
+    question = get_param(kv_array.kv_pairs, param_count, &param_name_question);
+    if (question != NULL && question->len > 0) {
+        if ((rc=fastken_index_search(r, &kv_array, body, question)) != NGX_OK) {
+            return rc;
+        }
+    } else {
+        fastken_add_param_to_kv_array(&kv_array, &param_name_display_answer,
+                &string_none);
+    }
 
     if (render_index_template(r, &kv_array, &output) != 0) {
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
@@ -573,8 +715,7 @@ static ngx_int_t fastken_index_do(ngx_http_request_t *r,
         return rc;
     }
 
-    return fken_send_reply_chunk(r, true,
-            output.str, output.len, false);
+    return fken_send_reply_chunk(r, true, &output, false);
 }
 
 static ngx_int_t ngx_http_fastken_index_handler(ngx_http_request_t *r)
@@ -605,7 +746,7 @@ static ngx_int_t ngx_http_fastken_index_handler(ngx_http_request_t *r)
 
     param_count = http_parse_url_params(cont.str, cont.len,
         params, MAX_PARAM_COUNT);
-    return fastken_index_do(r, params, param_count);
+    return fastken_index_do(r, body, params, param_count);
 }
 
 static void ngx_http_fastken_index_post_handler(ngx_http_request_t *r)
--- src/nginx-module/template.c
+++ src/nginx-module/template.c
@@ -137,32 +137,137 @@ static int load_index_template()
 }
 
 static int alloc_output_buffer(ngx_http_request_t *r,
-        const key_value_array_t *params, string_t *output)
+        const key_value_array_t *params, BufferInfo *buffer)
 {
     key_value_pair_t *kv;
     key_value_pair_t *kv_end;
     int value_len;
-    int buff_len;
-    int len;
 
     value_len = 0;
     kv_end = params->kv_pairs + params->count;
     for (kv=params->kv_pairs; kv<kv_end; kv++) {
-        len = kv->value.len - kv->key.len;
-        value_len += (len > 0) ? len : 0;
+        value_len += kv->value.len;
     }
 
-    buff_len = index_file_content.len + value_len;
-    logInfo("value_len: %d, buff_len: %d", value_len, buff_len);
+    buffer->length = 0;
+    buffer->alloc_size = index_file_content.len + value_len;
+    logInfo("value_len: %d, alloc_size: %d", value_len, buffer->alloc_size);
 
-    output->str = (char *)ngx_palloc(r->pool, sizeof(char) * buff_len);
-    if (output->str == NULL) {
+    buffer->buff = (char *)ngx_palloc(r->pool, buffer->alloc_size);
+    if (buffer->buff == NULL) {
         ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
-                "ngx_palloc %d bytes fail", buff_len);
+                "ngx_palloc %d bytes fail", buffer->alloc_size);
         return ENOMEM;
     }
 
     return 0;
+}
+
+static int check_realloc_output_buffer(ngx_http_request_t *r,
+        BufferInfo *buffer, const int inc_size)
+{
+    char *new_buff;
+    int expect_size;
+
+    expect_size = buffer->length + inc_size;
+    if (buffer->alloc_size >= expect_size) {
+        return 0;
+    }
+
+    while (buffer->alloc_size < expect_size) {
+        buffer->alloc_size *= 2;
+    }
+    logInfo("resize buff size to %d", buffer->alloc_size);
+
+    new_buff = (char *)ngx_palloc(r->pool, buffer->alloc_size);
+    if (new_buff == NULL) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                "ngx_palloc %d bytes fail", buffer->alloc_size);
+        return ENOMEM;
+    }
+
+    memcpy(new_buff, buffer->buff, buffer->length);
+    ngx_pfree(r->pool, buffer->buff);
+    buffer->buff = new_buff;
+    return 0;
+}
+
+static char *text_to_html(ngx_http_request_t *r, const string_t *value,
+        BufferInfo *buffer)
+{
+    const char *p;
+    const char *end;
+    char *dest;
+    char *buff_end;
+
+    logInfo("buffer length: %d, alloc_size: %d", buffer->length, buffer->alloc_size);
+
+    dest = buffer->buff + buffer->length;
+    buff_end = buffer->buff + buffer->alloc_size;
+    end = value->str + value->len;
+    for (p=value->str; p<end; p++) {
+        if (buff_end - dest < 8) {
+            buffer->length = dest - buffer->buff;
+            if (check_realloc_output_buffer(r, buffer, 8) != 0) {
+                return NULL;
+            }
+
+            dest = buffer->buff + buffer->length;
+            buff_end = buffer->buff + buffer->alloc_size;
+        }
+        switch (*p) {
+            case '<':
+                *dest++ = '&';
+                *dest++ = 'l';
+                *dest++ = 't';
+                *dest++ = ';';
+                break;
+            case '>':
+                *dest++ = '&';
+                *dest++ = 'g';
+                *dest++ = 't';
+                *dest++ = ';';
+                break;
+            case '&':
+                *dest++ = '&';
+                *dest++ = 'a';
+                *dest++ = 'm';
+                *dest++ = 'p';
+                *dest++ = ';';
+                break;
+            case '"':
+                *dest++ = '&';
+                *dest++ = 'q';
+                *dest++ = 'u';
+                *dest++ = 'o';
+                *dest++ = 't';
+                *dest++ = ';';
+                break;
+            case ' ':
+                *dest++ = '&';
+                *dest++ = 'n';
+                *dest++ = 'b';
+                *dest++ = 's';
+                *dest++ = 'p';
+                *dest++ = ';';
+                break;
+            case '\r':
+                break;  //igore \r
+            case '\n':
+                *dest++ = '<';
+                *dest++ = 'b';
+                *dest++ = 'r';
+                *dest++ = '>';
+                *dest++ = '\n';
+                break;
+            default:
+                *dest++ = *p;
+                break;
+        }
+    }
+
+    buffer->length = dest - buffer->buff;
+    return dest;
 }
 
 static int render_index_template(ngx_http_request_t *r,
@@ -172,24 +277,29 @@ static int render_index_template(ngx_http_request_t *r,
     int result;
     key_value_pair_t *kv;
     key_value_pair_t *kv_end;
+    BufferInfo buffer;
     string_t *value;
     char *p;
+    bool html_format;
 
-    if ((result=alloc_output_buffer(r, params, output)) != 0) {
+    if ((result=alloc_output_buffer(r, params, &buffer)) != 0) {
         return result;
     }
 
-    p = output->str;
+    p = buffer.buff;
     kv_end = params->kv_pairs + params->count;
     for (i=0; i<index_temp_node_array.count; i++) {
         if (index_temp_node_array.nodes[i].type == TEMPLATE_NODE_TYPE_STRING) {
             value = &index_temp_node_array.nodes[i].value;
+            html_format = false;
         } else {
             value = &empty_string;
+            html_format = false;
             for (kv=params->kv_pairs; kv<kv_end; kv++) {
                 if (fc_string_equal(&index_temp_node_array.nodes[i].value,
                             &kv->key))
                 {
+                    html_format = true;
                     value = &kv->value;
                     break;
                 }
@@ -197,10 +307,30 @@ static int render_index_template(ngx_http_request_t *r,
         }
 
         if (value->len > 0) {
-            memcpy(p, value->str, value->len);
-            p += value->len;
+            if (html_format) {
+                buffer.length = p - buffer.buff;
+                p = text_to_html(r, value, &buffer);
+                if (p == NULL) {
+                    return ENOMEM;
+                }
+            } else {
+                if ((p - buffer.buff) + value->len > buffer.alloc_size) {
+                    buffer.length = p - buffer.buff;
+                    if (check_realloc_output_buffer(r, &buffer, value->len) != 0) {
+                        return ENOMEM;
+                    }
+                    p = buffer.buff + buffer.length;
+                }
+                memcpy(p, value->str, value->len);
+                p += value->len;
+            }
         }
     }
-    output->len = p - output->str;
+    buffer.length = p - buffer.buff;
+
+    logInfo("buffer length: %d, alloc_size: %d", buffer.length, buffer.alloc_size);
+
+    output->str = buffer.buff;
+    output->len = buffer.length;
     return 0;
 }
